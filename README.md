b9b55eaff65b91d8d92e6beed4b676ca498dfbed
Kimaru thagana <macbuntu@localhost.localdomain>
2020-11-30 10:08:31 +0300

    setup django debug toolbar

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Learning to use redis with Django to cache querysets to increase performance. Pa
 2. Django Rest Framework
 3. Redis
 4. Docker
+5. [Loadtest](https://www.npmjs.com/package/loadtest)- Test api performance
--- dj_redis_cache/urls.py
+++ dj_redis_cache/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rest_api.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
