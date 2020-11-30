92dad7d52153182dd20301f0431cc32651e771ef
Kimaru thagana <macbuntu@localhost.localdomain>
2020-11-30 09:59:46 +0300

    setup views and urls

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', ),
+    path('', view_books, name='before_cache'),
 
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+from .models import *
 # Create your views here.
+
+
+@api_view(['GET'])
+def view_books(request):
+    products = Product.objects.all()
+    results = [product.to_json() for product in products]
+    return Response(results, status=status.HTTP_201_CREATED)
